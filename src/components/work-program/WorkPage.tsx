444f79b5e77f4c38071621686f1ec82b4900a711
giff <gifriend23@gmail.com>
2025-08-02 19:46:06 +0800

    chore: update picture on work program kawanua tumoutou and add program description

--- src/components/work-program/WorkPage.tsx
+++ src/components/work-program/WorkPage.tsx
@@ -7,16 +7,18 @@ import Modal from '../ui/modal';
 const baktiPrograms = [
   {
     title: 'Bakti Untuk Bumi',
-    imageSrc:
-      '/Bakti-untuk-Bumi-Biogass.JPG',
-    description: 'Dalam bakti untuk bumi ini, kami membuat biogas sebagai tempat untuk pengelolaan sampah organik, yang bisa menghasilkan gar untuk memasak, serta pupuk cair yang bisa digunakan untuk tanaman. Selain itu juga kami membuat tempat pemilah sampah berbasis Internet of Things (IoT) yang bisa menunjukan sampah organik atau anorganik.',
-    images: ['/Bakti-untuk-Bumi-Biogass.JPG'],
+    imageSrc: '/Bakti-untuk-Bumi-Biogass.JPG',
+    description:
+      'Dalam bakti untuk bumi ini, kami membuat biogas sebagai tempat untuk pengelolaan sampah organik, yang bisa menghasilkan gar untuk memasak, serta pupuk cair yang bisa digunakan untuk tanaman. Selain itu juga kami membuat tempat pemilah sampah berbasis Internet of Things (IoT) yang bisa menunjukan sampah organik atau anorganik.',
+    images: ['/Bakti-untuk-Bumi-Biogass.JPG',
+      '/bakti-untuk-bumi-2.JPG',
+    ],
   },
   {
     title: 'Bakti Mengalir',
-    imageSrc:
-      'https://blocks.astratic.com/img/general-img-square.png',
-    description: 'Bakti mengalir ini kami membuat filtrasi air yang bisa digunakan untuk mengalirkan air yang siap diminum untuk masyarakat maupun turis.',
+    imageSrc: '/bakti-mengalir-cover.JPG',
+    description:
+      'Bakti mengalir ini kami membuat filtrasi air yang bisa digunakan untuk mengalirkan air yang siap diminum untuk masyarakat maupun turis. Serta kami juga melakukan penyuluhan tentang sanitasi lingkungan',
     images: [
       'https://blocks.astratic.com/img/general-img-square.png',
       'https://blocks.astratic.com/img/general-img-square.png',
@@ -24,85 +26,55 @@ const baktiPrograms = [
   },
   {
     title: 'Bakti Cilik',
-    imageSrc:
-      'Bakti-Cilik-MPASI.JPG',
-    description: 'Cilik adalah singkatan dari Cegah stunting Lahirkan Insan Kuat dan program ini bertujuan untuk mencegah stunting pada anak-anak.',
-    images: [
-      '/Bakti-Cilik-MPASI.JPG',
-      '/Bakti-Cilik-MPASI.JPG',
-    ],
+    imageSrc: 'Bakti-Cilik-MPASI.JPG',
+    description:
+      'Cilik adalah singkatan dari Cegah stunting Lahirkan Insan Kuat dan program ini bertujuan untuk mencegah stunting pada anak-anak. Selain itu juga kami membuat aplikasi untuk pengingat posyandu, guidebook serta chatbot dab juga kami membuat game untuk edukasi tentang gizi',
+    images: ['/bakti-cilik-1.JPG', '/bakti-cilik-2.JPG'],
   },
   {
     title: 'Bakti Bersih',
-    imageSrc:
-      'https://blocks.astratic.com/img/general-img-square.png',
-    description: 'Program ini berfokus pada edukasi tentang pentingnya kebersihan gigi dan tangan dengan target anak sekolah dasar sehingga mereka dapat mengimplementasikan kebersihan diri sejak dini.',
-    images: [
-      'https://via.placeholder.com/300',
-      'https://via.placeholder.com/300x200',
-    ],
+    imageSrc: 'bakti-bersih-cover.JPG',
+    description:
+      'Program ini berfokus pada edukasi tentang pentingnya kebersihan gigi dan tangan dengan target anak sekolah dasar sehingga mereka dapat mengimplementasikan kebersihan diri sejak dini.',
+    images: ['bakti-bersih-1.JPG', 'bakti-bersih-2.JPG'],
   },
   {
     title: 'Bakti Bergizi',
-    imageSrc:
-      'https://blocks.astratic.com/img/general-img-square.png',
-    description: 'Program ini bertujuan untuk meningkatkan kesadaran akan pentingnya gizi seimbang dan pola makan sehat dengan target yang kami sasar adalah remaja dalam sekolah menengah pertama.',
-    images: [
-      'https://via.placeholder.com/300',
-      'https://via.placeholder.com/300x200',
-    ],
+    imageSrc: 'bakti-bergizi-cover.JPG',
+    description:
+      'Program ini bertujuan untuk meningkatkan kesadaran akan pentingnya gizi seimbang dan pola makan sehat dengan target yang kami sasar adalah remaja dalam sekolah menengah pertama.',
+    images: ['bakti-bergizi-1.JPG', 'bakti-bergizi-2.JPG'],
   },
   {
     title: 'Bakti Bergerak',
-    imageSrc:
-      '/Bakti-Bergerak.JPG',
-    description: 'Penjelasan lengkap tentang Bakti Bergerak...',
-    images: [
-      '/Bakti-Bergerak.JPG',
-      '/Bakti-Bergerak.JPG',
-    ],
+    imageSrc: '/bakti-bergerak-cover.JPG',
+    description: 'Bakti bergerak ini bertujuan untuk menambah semangat masyarakat dalam berolahraga, salah satunya dengan cara mengadakan senam bersama di lapangan terbuka.',
+    images: ['bakti-bergerak-1.JPG', 'bakti-bergerak-2.JPG'],
   },
   {
     title: 'Bakti Entaskan DBD & Malaria',
-    imageSrc:
-      '/Bakti-Entaskan-DBD.JPG',
-    description: 'Penjelasan lengkap tentang Bakti Entaskan DBD & Malaria...',
-    images: [
-      '/Bakti-Entaskan-DBD.JPG',
-      'Bakti-Entaskan-DBD.JPG',
-    ],
+    imageSrc: '/Bakti-Entaskan-DBD.JPG',
+    description: 'Program ini bertujuan untuk mengedukasi masyarakat tentang bahaya DBD dan malaria, serta cara pencegahannya. Kami juga melakukan edukasi agar masyarakat dapat menjadi JUMANTIK (Juru Pemantau Jentik).',
+    images: ['/bakti-entaskan-dbd-1.JPG', '/bakti-entaskan-dbd-2.JPG'],
   },
   {
     title: 'Bakti Bebas TB',
-    imageSrc:
-      '/Bakti-Bebas-TB.jpg',
-    description: 'Penjelasan lengkap tentang Bakti Bebas TB...',
-    images: [
-      '/Bakti-Bebas-TB.jpg',
-      '/Bakti-Bebas-TB.jpg',
-    ],
+    imageSrc: '/Bakti-Bebas-TB.jpg',
+    description: 'Kami melakukan edukasi pengenalan tetang TB, cara penularan, gejala, serta pencegahan penyakit TB.',
+    images: ['/Bakti-Bebas-TB.jpg'],
   },
   {
     title: 'Bakti Bahari',
-    imageSrc:
-      'https://blocks.astratic.com/img/general-img-square.png',
-    description: 'Penjelasan lengkap tentang Bakti Bahari...',
-    images: [
-      'https://via.placeholder.com/300',
-      'https://via.placeholder.com/300x200',
-    ],
+    imageSrc: 'bakti-bahari-cover.JPG',
+    description: 'Dalam bakti bahari ini, kami melakukan edukasi serta pemberian leflet tentang penyakit-penyakit yang bisa terkena ketika berwisaa di daerah pesisir pantai. Selain itu kami juga melakukan pemeriksaan kesehatan gratis',
+    images: ['bakti-bahari-1.JPG', 'bakti-bahari-2.JPG'],
   },
   {
     title: 'Bakti Hijau',
-    imageSrc:
-      'https://blocks.astratic.com/img/general-img-square.png',
+    imageSrc: 'bakti-hijau-cover.JPG',
     description: 'Penjelasan lengkap tentang Bakti Hijau...',
-    images: [
-      'https://via.placeholder.com/300',
-      'https://via.placeholder.com/300x200',
-    ],
+    images: ['bakti-hijau-1.JPG'],
   },
-  
 ];
 
 const WorkPage: React.FC = () => {
@@ -121,7 +93,7 @@ const WorkPage: React.FC = () => {
           Program Kerja Tim Kawanua Tumou Tou
         </ScrollReveal>
       </div>
-    
+
       <div className="flex flex-wrap justify-center gap-10 px-4 py-8">
         {baktiPrograms.map((program) => (
           <div
@@ -157,18 +129,27 @@ const WorkPage: React.FC = () => {
           onClose={() => setActiveProgram(null)}
           title={activeProgram.title}
           description={activeProgram.description}>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            {activeProgram.images.map((src, idx) => (
+          {activeProgram.images.length === 1 ? (
+            <div className="flex justify-center">
               <img
-                key={idx}
-                src={src}
-                alt={`Foto ${idx + 1}`}
-                className="rounded shadow"
-                width={300}
-                height={200}
+                src={activeProgram.images[0]}
+                alt="Foto 1"
+                className="rounded shadow max-w-full h-auto"
+                style={{ width: '300px', height: '200px', objectFit: 'cover' }}
               />
-            ))}
-          </div>
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              {activeProgram.images.map((src, idx) => (
+                <img
+                  key={idx}
+                  src={src}
+                  alt={`Foto ${idx + 1}`}
+                  className="rounded shadow w-full h-48 object-cover"
+                />
+              ))}
+            </div>
+          )}
         </Modal>
       )}
     </div>
